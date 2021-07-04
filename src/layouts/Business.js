e848b042fe62dc15d0c639748e5b732684f01a3a
vuongvgc <vuongvgc@gmail.com>
2021-07-04 10:42:38 +0700

    create business layout

--- src/layouts/Business.js
+++ src/layouts/Business.js
@@ -1,4 +1,23 @@
 function Business() {
-  return <div>Business</div>;
+  return (
+    <section className="business__box">
+      <div className="business__left">
+        <img
+          src="./img/dog-ceo-zine.jpg"
+          alt="Dog CEO Zine"
+          className="business__img"
+        />
+      </div>
+      <div className="business__right">
+        <h2 className="business__header">Dog CEO Zine</h2>
+        <p className="business__text">
+          A business and lifestyle magazine for the modern dog.
+        </p>
+        <p className="business__text">
+          Issue 1 out now. Buy now from Side Orders Publishing. Ships worldwide.
+        </p>
+      </div>
+    </section>
+  );
 }
 export default Business;
